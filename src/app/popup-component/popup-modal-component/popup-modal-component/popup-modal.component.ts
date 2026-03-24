d86433441e1472d214a1522beb92e38e2392bbe3
Izhan Lara <izhan.lara@w2m.com>
2026-03-24 13:34:17 +0100

    Migramos ngFor por For. Use: ng generate @angular/core:control-flow

--- src/app/popup-component/popup-modal-component/popup-modal-component/popup-modal.component.ts
+++ src/app/popup-component/popup-modal-component/popup-modal-component/popup-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, Injectable } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,7 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   selector: 'app-popup-modalEdit',
   templateUrl: './popup-modal.component.html',
   styleUrls: ['../popup-modal.component.scss'],
-  imports: [CommonModule, MatButtonModule, MatSelectModule, MatFormFieldModule],
+  imports: [MatButtonModule, MatSelectModule, MatFormFieldModule],
 })
 @Injectable({
   providedIn: 'root',
