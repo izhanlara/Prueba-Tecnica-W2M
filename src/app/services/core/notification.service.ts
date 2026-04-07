1342286c7f25b8ed34e4bc7e84ff2c8616c571c4
Izhan Lara <izhan.lara@w2m.com>
2026-04-07 11:06:09 +0200

    delete imports

--- src/app/services/core/notification.service.ts
+++ src/app/services/core/notification.service.ts
@@ -1,6 +1,5 @@
 import { inject, Injectable } from '@angular/core';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
 
 type NotificationType = 'success' | 'error';
 
