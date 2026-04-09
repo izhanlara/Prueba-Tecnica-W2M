72322d20f3f73702bc2cd25d461579809e669bae
Izhan Lara <izhan.lara@w2m.com>
2026-04-09 09:33:25 +0200

    Cambiamos a input signals

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,5 +1,5 @@
 import { TitleCasePipe } from '@angular/common';
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Hero } from '@core/heroes.model';
@@ -13,18 +13,17 @@ import { ModalEditService } from '@services/core/modal-services/modal-edit.servi
   imports: [TitleCasePipe, MatIconModule, MatButtonModule],
 })
 export class CardComponent {
-  @Input() card!: Hero;
-  @Input() index!: number;
-
+  public readonly card = input.required<Hero>();
+  public readonly index = input.required<number>();
   private readonly modalEditService = inject(ModalEditService);
   private readonly modalDeleteService = inject(ModalDeleteService);
 
   public editHeroModal() {
-    return this.modalEditService.openModalEdit(this.card, this.index);
+    return this.modalEditService.openModalEdit(this.card(), this.index());
   }
 
   public deleteHeroModal() {
-    this.modalDeleteService.selectedHeroIndex.set(this.index);
+    this.modalDeleteService.selectedHeroIndex.set(this.index());
     this.modalDeleteService.openModalDelete();
   }
 }
