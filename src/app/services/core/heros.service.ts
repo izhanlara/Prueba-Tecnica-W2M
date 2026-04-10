99ca5771841692f9de481f008b5680cb97030541
Izhan Lara <izhan.lara@w2m.com>
2026-04-10 10:41:44 +0200

    Feature: Añadimos modales de error

--- src/app/services/core/heros.service.ts
+++ src/app/services/core/heros.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { catchError, Observable, tap } from 'rxjs';
 import { Hero } from './heroes.model';
 import { NotificationService } from './notification.service';
 
@@ -13,13 +13,22 @@ export class HerosJson {
   private readonly API_URL = '/allHeros';
 
   public getHeros(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.API_URL);
+    return this.http.get<Hero[]>(this.API_URL).pipe(
+      catchError((error) => {
+        this.snackBar.messages('Error al cargar los héroes', 'error');
+        throw error;
+      }),
+    );
   }
 
   public updateHero(id: number, updatedHero: Hero): Observable<Hero> {
     return this.http.put<Hero>(`${this.API_URL}/${id}`, updatedHero).pipe(
       tap(() => {
         this.snackBar.messages('Héroe actualizado correctamente');
+      }),
+      catchError((error) => {
+        this.snackBar.messages('Error al actualizar el héroe', 'error');
+        throw error;
       }),
     );
   }
@@ -29,6 +38,10 @@ export class HerosJson {
       tap(() => {
         this.snackBar.messages('Héroe eliminado correctamente');
       }),
+      catchError((error) => {
+        this.snackBar.messages('Error al eliminar el héroe', 'error');
+        throw error;
+      }),
     );
   }
 
@@ -36,6 +49,10 @@ export class HerosJson {
     return this.http.post<Hero>(`${this.API_URL}`, newHero).pipe(
       tap(() => {
         this.snackBar.messages('Héroe agregado correctamente');
+      }),
+      catchError((error) => {
+        this.snackBar.messages('Error al agregar el héroe', 'error');
+        throw error;
       }),
     );
   }
